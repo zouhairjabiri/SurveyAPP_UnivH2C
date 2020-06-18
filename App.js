9f8a3664284cabd806d28fa2990540d3c90b919e
zouhairjabiri <zouhairjab0@gmail.com>
2020-06-18 14:38:10 +0100

    delte
    delt

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Navigation from './Components/Navigation'
-function App() {
-  return (
-    <Navigation />
-  );
-}
-
-export default App;
--- Components/assets/Colors.js
+++
@@ -1,9 +0,0 @@
-const COLOR = {
-	DBLUE: '#0e3e82',
-	BLUE: '#245591',
-	GREY: '#d3d0d2',
-	RED: '#FF0000',
-	WHITE: '#FFFFFF',
-}
-
-module.exports = COLOR
